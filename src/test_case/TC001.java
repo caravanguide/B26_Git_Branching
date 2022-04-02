477b9b45ec62d772607122ce402f6c76395dc92d
Rashidkurban <98862848+caravanguide@users.noreply.github.com>
2022-04-02 11:37:50 -0400

    updated  TC001

--- src/test_case/TC001.java
+++ src/test_case/TC001.java
@@ -3,6 +3,6 @@ package test_case;
 public class TC001 {
     public static void main(String[] args) {
         // testing some thing on the first day
-        //
+        // more testing on  this new project
     }
 }

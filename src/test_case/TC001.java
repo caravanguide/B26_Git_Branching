2a9351b7d53fd88a9e6075289a74b85616832b91
Rashidkurban <98862848+caravanguide@users.noreply.github.com>
2022-04-02 11:55:34 -0400

    eddit negetive testing TC001

--- src/test_case/TC001.java
+++ src/test_case/TC001.java
@@ -4,5 +4,9 @@ public class TC001 {
     public static void main(String[] args) {
         // testing some thing on the first day
         // more testing on  this new project
+
+        //negative testing
+
+
     }
 }
